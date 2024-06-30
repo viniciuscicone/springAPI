26ab3ac481ca9e5fe1741e1b0dcd62dfb9c9f6f3
vinicius <viniciuscicone@gmail.com>
2024-06-30 17:16:10 -0300

    update

--- src/main/java/com/booksproject/api/ApiApplication.java
+++ src/main/java/com/booksproject/api/ApiApplication.java
@@ -2,12 +2,22 @@ package com.booksproject.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@RestController
+//http://localhost:8080/hello?name=teste
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
