9f22f38b06243da2b1d3edb154d8762d41492207
vinicius <viniciuscicone@gmail.com>
2024-07-01 20:59:19 -0300

    flyAway

--- src/main/java/com/booksproject/api/ApiApplication.java
+++ src/main/java/com/booksproject/api/ApiApplication.java
@@ -2,14 +2,14 @@ package com.booksproject.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 @RestController
+
 //http://localhost:8080/hello?name=teste
 public class ApiApplication {
 
---
+++ src/main/java/com/booksproject/api/controllers/LibController.java
@@ -0,0 +1,17 @@
+package com.booksproject.api.controllers;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class LibController {
+    @GetMapping
+    public ResponseEntity getAllBooks() {
+        return ResponseEntity.ok("Deu ok");
+    }
+}
+
