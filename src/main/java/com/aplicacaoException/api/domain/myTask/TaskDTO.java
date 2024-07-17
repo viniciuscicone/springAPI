77e262488dcee07b1e3489c8568284a2ba26e4b4
Vinicius Barbosa <viniciuscicone@gmail.com>
2024-07-16 23:31:23 -0300

    ref

--- src/main/java/com/aplicacaoException/api/domain/myTask/TaskDTO.java
+++ src/main/java/com/aplicacaoException/api/domain/myTask/TaskDTO.java
@@ -5,8 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
-import org.yaml.snakeyaml.events.Event;
 
 
 public record TaskDTO (
